f096f8bf37a7921fc73ed6c7bfc2ddeda2117d44
Maxim Gavrilenko <gavrilenko@insystem.ru>
2017-09-25 11:13:56 +0300

    Redesigned input form

--- pages/index.vue
+++ pages/index.vue
@@ -4,48 +4,49 @@
 
       <div class="columns">
         <div class="column">
-          <form action="">
-            <div class="field">
-              <label class="label">Наименование</label>
-              <div class="control">
-                <input class="input" type="text" placeholder="Наименование">
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Количество</label>
-              <div class="field has-addons">
-                <p class="control">
-                  <input class="input" type="number" placeholder="Количество">
-                </p>
-                <p class="control">
-                  <a class="button is-static">
-                    шт
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div class="field">
-              <label class="label">Цена</label>
-              <div class="field has-addons">
-                <p class="control">
-                  <input class="input" type="number" placeholder="Стоимость">
-                </p>
-                <p class="control">
-                  <a class="button is-static">
-                    &#8381
-                  </a>
-                </p>
-              </div>
-            </div>
-
+          <div class="field">
+            <label class="label">Наименование</label>
             <div class="control">
-              <button class="button is-primary">Добавить</button>
+              <input class="input" type="text" placeholder="Наименование" v-model="name"
+                     @focus="$event.target.select()">
             </div>
-          </form>
+          </div>
+          <div class="field">
+            <label class="label">Количество</label>
+            <div class="field has-addons">
+              <p class="control">
+                <input class="input" type="number" placeholder="Количество" v-model.number="count"
+                       @focus="$event.target.select()">
+              </p>
+              <p class="control">
+                <a class="button is-static">
+                  шт
+                </a>
+              </p>
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Цена</label>
+            <div class="field has-addons">
+              <p class="control">
+                <input class="input" type="number" placeholder="Стоимость" v-model.number="price"
+                       @focus="$event.target.select()">
+              </p>
+              <p class="control">
+                <a class="button is-static">
+                  &#8381
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div class="control">
+            <button class="button is-primary" @click="addRecord">Добавить</button>
+          </div>
         </div>
 
         <div class="column">
-          <table class="table is-fullwidth">
+          <table class="table is-fullwidth" v-show="records.length">
             <thead>
             <tr>
               <th>№</th>
@@ -87,46 +88,25 @@
   export default {
     data() {
       return {
-        records: [
-          {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          }, {
-            name: 'hejf hE fe fwef',
-            count: 10,
-            price: 20,
-          },
-        ],
+        name: '',
+        count: 0,
+        price: 0.0,
+        records: [],
       };
     },
     methods: {
       removeRecord(rec) {
         this.records.splice(this.records.indexOf(rec), 1);
+      },
+      addRecord() {
+        this.records.push({
+          name: this.name,
+          count: this.count,
+          price: this.price,
+        });
+        this.name = '';
+        this.count = 0;
+        this.price = 0;
       },
     },
   };
